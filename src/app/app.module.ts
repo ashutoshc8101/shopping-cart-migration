6a1bafbf4c86494299bad7025883fd1311f9517e
ashutoshc8101 <ashutoshc8101@gmail.com>
2021-03-08 15:42:25 +0530

    dual-boot

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+@NgModule({
+    imports: [
+        CommonModule,
+        BrowserModule
+    ]
+})
+export class AppModule {
+
+}
